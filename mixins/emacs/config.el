7b89c51033363b11f0a3fa403668366968e98f1a
Eduardo Correia <skelozard1@gmail.com>
2025-09-10 00:49:33 +0100

    Update Emacs configuration
    - Set Emacs as the default editor
    - Use emacs-nox package (CLI only)

--- mixins/emacs/config.el
+++ mixins/emacs/config.el
@@ -12,3 +12,27 @@
 
 ;; Display line numbers
 (global-display-line-numbers-mode 1)
+
+(global-font-lock-mode 1)             ;; Turn on for all buffers
+(setq font-lock-maximum-decoration t) ;; Use maximum decoration for syntax highlighting
+
+;; Basic associations for built-in and installed modes
+(setq auto-mode-alist
+      (append
+       '(("\\.py\\'"      . python-mode)
+         ("\\.js\\'"      . js-mode)
+         ("\\.ts\\'"      . typescript-mode)
+         ("\\.jsx\\'"     . js-jsx-mode)
+         ("\\.c\\'"       . c-mode)
+         ("\\.cpp\\'"     . c++-mode)
+         ("\\.java\\'"    . java-mode)
+         ("\\.go\\'"      . go-mode)
+         ("\\.rs\\'"      . rust-mode)
+         ("\\.md\\'"      . markdown-mode)
+         ("\\.yaml\\'"    . yaml-mode)
+         ("\\.yml\\'"     . yaml-mode)
+         ("\\.html?\\'"   . html-mode)
+         ("\\.css\\'"     . css-mode)
+         ("\\.json\\'"    . json-mode)
+         ("\\.sh\\'"      . sh-mode))
+       auto-mode-alist))
--- mixins/emacs/default.nix
+++ mixins/emacs/default.nix
@@ -1,19 +1,23 @@
-{ user, ... }:
+{ user, pkgs, ... }:
 
 {
   home-manager.users.${user} = {
     programs.emacs = {
       enable = true;
+      package = pkgs.emacs-nox;
       extraConfig = builtins.readFile ./config.el;
-      extraPackages = epkgs: with epkgs; [
-        # Evil mode
-        evil
-        evil-collection
-        evil-surround
-        evil-commentary
-        # Auto-completion
-        corfu
-      ];
+      extraPackages =
+        epkgs: with epkgs; [
+          # Evil mode
+          evil
+          evil-collection
+          evil-surround
+          evil-commentary
+          # Auto-completion
+          corfu
+        ];
     };
   };
+
+  environment.variables.EDITOR = "emacs";
 }
